ba1a4410d6fce78a0ef580a815fe87cb5a65f04f
zeyulin <zeyulin@clubfactory.com>
2019-08-28 10:21:02 +0800

    test

--- app/router/test1.js
+++ app/router/test1.js
@@ -2,7 +2,7 @@ var http = require('http')
 var express = require("express");
 var api = express.Router();
 const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(200, 200)
+const canvas = createCanvas(400, 400)
 const ctx = canvas.getContext('2d')
 var _ = require("lodash");
 api.get("/draw/:num", function(req, res) {
@@ -15,9 +15,10 @@ api.get("/draw/:num", function(req, res) {
       const parsedData = JSON.parse(rawData);
       const cover = _.get(parsedData, 'body.missionRecord.product.productTemplate.coverImgLow')
       loadImage(cover).then((image) => {
-        ctx.drawImage(image, 0, 0, 200, 200)
-        ctx.font = '30px Impact'
-        ctx.fillText('Awesome!', 50, 100)
+        ctx.drawImage(image, 0, 0, 400, 400)
+        ctx.font = '30px Impact #'
+        ctx.fillStyle = "Red";
+        ctx.fillText('Group Lottery', 50, 100)
         var text = ctx.measureText('Awesome!')
         ctx.strokeStyle = 'rgba(0,0,0,0.5)'
         ctx.beginPath()
